e1d9188fcc0c68f30e184e3940632217954fea0d
Amri Rosadi <amriroxadi@gmail.com>
2021-07-30 09:47:40 +0800

    review scheme

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,1 +1,34 @@
 // Amri's Code
+const express = require("express");
+
+// Import auth
+const { admin, user } = require("../middlewares/auth");
+
+// Import validator
+const {
+  createOrUpdateReviewValidator,
+  getDetailReview,
+} = require("../middlewares/validators/review");
+
+// Import controller
+const {
+  createReview,
+  getAllReview,
+  getDetailReview,
+  updateReview,
+  deleteReview,
+} = require("../controllers/review");
+
+// Make router
+const router = express.Router();
+
+// Make some routes
+router.post("/", createOrUpdateReviewValidator, createReview);
+router.get("/", getAllReview);
+
+router.get("/:id", getDetailReview, getDetailReview);
+router.put("/:id", createOrUpdateReviewValidator, updateReview);
+router.delete("/:id", deleteReview);
+
+// Exports
+module.exports = router;
