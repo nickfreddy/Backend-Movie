33ecf44c61fcadbf6f1faa8bc56168d16ead1308
Pandji Bayu <bayu.pandjisatria@gmail.com>
2021-07-30 08:39:56 +0700

    add movie model

--- models/movie.js
+++ models/movie.js
@@ -1,1 +1,52 @@
-// Bayu's Code
+const mongoose = require("mongoose");
+
+const movieSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    genre: [
+      { type: mongoose.Schema.Types.ObjectId, require: true, ref: "category" },
+    ],
+    release_year: {
+      type: String,
+      required: false,
+    },
+    poster: {
+      type: String,
+      required: false,
+      get: getPoster,
+    },
+    trailer: {
+      type: String,
+      required: false,
+    },
+    synopsis: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "rating",
+    },
+    review: [{ type: mongoose.Schema.Types.ObjectId, ref: "review" }],
+  },
+  {
+    timestamps: {
+      createdAt: "createdAt",
+      updatedAt: "updatedAt",
+    },
+    toJSON: { getters: true },
+  }
+);
+
+function getPoster(poster) {
+  if (!poster || poster.includes("https") || poster.includes("http")) {
+    return poster;
+  }
+
+  return `/images/poster/${poster}`;
+}
+
+module.exports = mongoose.model("movie", movieSchema);
